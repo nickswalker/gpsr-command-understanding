c59ccc58a1dc65204d4c46d369a0eab3000a6dfd
Yu-Tang Peng <pengy25@cs.washington.edu>
2019-03-21 19:58:07 -0700

    Tuned down models to keep
    Prepare more exp files
    Fixed fine tune script bug

--- scripts/fine_tune_all_models.sh
+++ scripts/fine_tune_all_models.sh
@@ -1,10 +1,10 @@
 # Models to be trained
 models=("all_data_all_cats" "bert_base_seq2seq_freezed" "bert_large_seq2seq_freezed" "elmo_seq2seq_freezed" "openai_seq2seq_freezed" "seq2seq")
 
-for model in ${models[*]}:
+for model in ${models[*]}
 do
   for i in {1..10} "Full"
   do
-    allennlp fine-tune -m results/$model/model.tar.gz -c experiments/fine_tune_$i.json -s fine_tune_results/${model}_$i --include-package gpsr_semantic_parser
+    allennlp fine-tune -m results/$model/model.tar.gz -c experiments/${model}_$i.json -s fine_tune_results/${model}_$i --include-package gpsr_semantic_parser
   done
 done
