efafbb5f878213602ebf22d876471380799fb61d
Yu-Tang Peng <pengy25@cs.washington.edu>
2019-03-21 19:15:04 -0700

    Tweak experiments
    Tuned down models to keep
    Added missing parameter
    Prepare more exp files

--- scripts/fine_tune_all_models.sh
+++ scripts/fine_tune_all_models.sh
@@ -3,8 +3,8 @@ models=("all_data_all_cats" "bert_base_seq2seq_freezed" "bert_large_seq2seq_free
 
 for model in ${models[*]}:
 do
-  for i in {1..10} Full
+  for i in {1..10} "Full"
   do
-    allennlp fine-tune -m results/$model/model.tar.gz -c experiments/fine_tune_$i.json -s fine_tune_results/${model}_$i
+    allennlp fine-tune -m results/$model/model.tar.gz -c experiments/fine_tune_$i.json -s fine_tune_results/${model}_$i --include-package gpsr_semantic_parser
   done
 done
