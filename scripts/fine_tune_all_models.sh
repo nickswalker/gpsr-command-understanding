9f1d16c6745377b23349a6850007aa8731ce1336
Yu-Tang Peng <pengy25@cs.washington.edu>
2019-04-12 20:20:08 -0700

    Small edit on script for fine tune

--- scripts/fine_tune_all_models.sh
+++ scripts/fine_tune_all_models.sh
@@ -6,6 +6,6 @@ do
   for i in {1..8} "Full"
   do
     allennlp fine-tune -m results/$model/model.tar.gz -c experiments/${model}_$i.json -s fine_tune_results/${model}_$i --include-package gpsr_semantic_parser
-    echo "$model is done" >> progress.txt
+    echo "${model}_$i is done" >> progress.txt
   done
 done
