68056cdb6760fc54cbe111df8cc63304ea4d937c
Yu-Tang Peng <pengy25@cs.washington.edu>
2019-04-19 11:54:20 -0700

    Added script to generate vocabulary

---
+++ scripts/make_vocabulary.sh
@@ -0,0 +1,11 @@
+if [[ $# < 2 ]]; then
+    echo "Please indicate the training and validation path"
+    exit 1
+fi
+
+training_path=$1
+validation_path=$2
+override_config="{\"train_data_path\":\"${training_path}\",\"validation_data_path\":\"${validation_path}\"}"
+
+echo $override_config
+allennlp dry-run experiments/seq2seq.json -s vocabulary -o ${override_config} --include-package gpsr_semantic_parser
