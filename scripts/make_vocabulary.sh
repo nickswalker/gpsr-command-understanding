b23ddb0f28ac02133df4f009c93755e9ac86c750
Yu-Tang Peng <pengy25@cs.washington.edu>
2019-04-20 03:44:13 -0700

    Fixed bug in vocab generation

--- scripts/make_vocabulary.sh
+++ scripts/make_vocabulary.sh
@@ -14,4 +14,4 @@ validation_path=$2
 override_config="{\"train_data_path\":\"${training_path}\",\"validation_data_path\":\"${validation_path}\"}"
 
 echo $override_config
-allennlp dry-run experiments/seq2seq.json -s vocabulary -o ${override_config} --include-package gpsr_semantic_parser
+allennlp dry-run experiments/seq2seq.json -s ./ -o ${override_config} --include-package gpsr_semantic_parser
--- scripts/simple_train_all_models.sh
+++ scripts/simple_train_all_models.sh
@@ -1,3 +1,10 @@
+echo "Cleaning all training data..."
+if [ -d "results" ]; then
+    rm -rf results
+    mkdir results
+fi
+echo "Done."
+
 config="{\"train_data_path\":\"data/cat1/train.txt\",\"validation_data_path\":\"data/cat1/val.txt\",\"vocabulary\":{\"directory_path\":\"vocabulary\"}}"
 
 bash scripts/train_all_models results experiments -- -o $config
