4fbb7d08bb98fa9777273c4fb6c049fbd566f7f9
Yu-Tang Peng <pengy25@cs.washington.edu>
2019-04-20 03:17:51 -0700

    Finalize dataset names

--- scripts/make_all_dataset.sh
+++ scripts/make_all_dataset.sh
@@ -1,25 +1,25 @@
 echo "Cleaning old data..."
-if [ -d "data/1_1_a" ]; then
+if [ -d "data/cat1" ]; then
     rm -rf data/1_1
 fi
 
-if [ -d "data/2_2_a" ]; then
+if [ -d "data/cat2" ]; then
     rm -rf data/2_2
 fi
 
-if [ -d "data/3_3_a" ]; then
+if [ -d "data/cat3" ]; then
     rm -rf data/3_3
 fi
 
-if [ -d "data/123_123_a" ]; then
+if [ -d "data/all" ]; then
     rm -rf data/123_123
 fi
 echo "Done."
 
 echo "Generate data..."
-python -m gpsr_semantic_parser.data.make_dataset -s 0.8 0.2 0.0 -trc 1 -tc 1
-python -m gpsr_semantic_parser.data.make_dataset -s 0.4 0.1 0.5 -trc 2 -tc 2 -i
-python -m gpsr_semantic_parser.data.make_dataset -s 0.4 0.1 0.5 -trc 3 -tc 3 -i
-python -m gpsr_semantic_parser.data.make_dataset -s 0.8 0.2 0.0
+python -m gpsr_semantic_parser.data.make_dataset -name cat1 -s 0.8 0.2 0.0 -trc 1 -tc 1
+python -m gpsr_semantic_parser.data.make_dataset -name cat2 -s 0.4 0.1 0.5 -trc 2 -tc 2 -i
+python -m gpsr_semantic_parser.data.make_dataset -name cat3 -s 0.4 0.1 0.5 -trc 3 -tc 3 -i
+python -m gpsr_semantic_parser.data.make_dataset -name all -s 0.8 0.2 0.0
 echo "Done."
 
