519410bd2f50234f1810dfde1f47ef76676c0f7a
Yu-Tang Peng <pengy25@cs.washington.edu>
2019-04-19 21:00:39 -0700

    Fixed script bug

--- scripts/make_all_dataset.sh
+++ scripts/make_all_dataset.sh
@@ -17,9 +17,9 @@ fi
 echo "Done."
 
 echo "Generate data..."
-python -m gpsr_semantic_parser/data/make_dataset -s 0.8 0.2 0.0 -trc 1 -tc 1
-python -m gpsr_semantic_parser/data/make_dataset -s 0.4 0.1 0.5 -trc 2 -tc 2 -i
-python -m gpsr_semantic_parser/data/make_dataset -s 0.4 0.1 0.5 -trc 3 -tc 3 -i
-python -m gpsr_semantic_parser/data/make_dataset -s 0.8 0.2 0.0
+python -m gpsr_semantic_parser.data.make_dataset -s 0.8 0.2 0.0 -trc 1 -tc 1
+python -m gpsr_semantic_parser.data.make_dataset -s 0.4 0.1 0.5 -trc 2 -tc 2 -i
+python -m gpsr_semantic_parser.data.make_dataset -s 0.4 0.1 0.5 -trc 3 -tc 3 -i
+python -m gpsr_semantic_parser.data.make_dataset -s 0.8 0.2 0.0
 echo "Done."
 
