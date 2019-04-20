bd8816f1a75903922e846f7f03e24dfc5cc87110
Yu-Tang Peng <pengy25@cs.washington.edu>
2019-04-20 03:55:18 -0700

    Changed directory setting

--- scripts/fine_tune_all_models.sh
+++ scripts/fine_tune_all_models.sh
@@ -6,9 +6,9 @@ do
   for i in {1..8}
   do
     config="{\"train_data_path\":\"data/cat2/train${i}.txt\",\"validation_data_path\":\"data/cat2/val.txt\"}"
-    allennlp fine-tune -m results/$model/model.tar.gz -c experiments/${model}.json -s fine_tune_results_cat2/${model}_$i -o $config --include-package gpsr_semantic_parser
+    allennlp fine-tune -m experiments/results_cat1/$model/model.tar.gz -c experiments/${model}.json -s fine_tune_results_cat2/${model}_$i -o $config --include-package gpsr_semantic_parser
     echo "${model}_$i is done" >> fine_tune_progress_cat2.txt
   done
   config="{\"train_data_path\":\"data/cat2/train.txt\",\"validation_data_path\":\"data/cat2/val.txt\"}"
-  allennlp fine-tune -m results/$model/model.tar.gz -c experiments/${model}.json -s fine_tune_results_cat2/${model} -o $config --include-package gpsr_semantic_parser
+  allennlp fine-tune -m experiments/results_cat1/$model/model.tar.gz -c experiments/${model}.json -s fine_tune_results_cat2/${model} -o $config --include-package gpsr_semantic_parser
 done
--- scripts/simple_train_all_models.sh
+++ scripts/simple_train_all_models.sh
@@ -1,10 +1,9 @@
 echo "Cleaning all training data..."
-if [ -d "results" ]; then
-    rm -rf results
-    mkdir results
+if [ -d "experiments/results_cat1" ]; then
+    rm -rf experiments/results_cat1
 fi
 echo "Done."
 
 config="{\"train_data_path\":\"data/cat1/train.txt\",\"validation_data_path\":\"data/cat1/val.txt\",\"vocabulary\":{\"directory_path\":\"vocabulary\"}}"
 
-bash scripts/train_all_models results experiments -- -o $config
+bash scripts/train_all_models cat1 experiments -- -o $config
