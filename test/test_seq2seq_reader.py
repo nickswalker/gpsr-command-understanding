dca3855078353b58d4013d5d36c873d9fa3b813c
Nick Walker <nick@nickwalker.us>
2019-05-15 21:54:53 -0700

    Add CI testing
    Reformat test module to make it possible to use unittest discover

--- test/test_seq2seq_reader.py
+++ test/test_seq2seq_reader.py
@@ -1,14 +1,20 @@
+import os
+from os.path import join
+
 from allennlp.common.testing import AllenNlpTestCase
 from allennlp.common.util import ensure_list
 from gpsr_semantic_parser.models.seq2seq_data_reader import Seq2SeqDatasetReader
+
+
+FIXTURE_DIR = os.path.join(os.path.dirname(__file__), "fixtures")
 
 class TestSeq2SeqReader(AllenNlpTestCase):
 
     def setUp(self):
         super(TestSeq2SeqReader, self).setUp()
         self.reader = Seq2SeqDatasetReader()
-        instances = self.reader.read("fixtures/train.txt")
+        instances = self.reader.read(join(FIXTURE_DIR,"train.txt"))
         self.instances = ensure_list(instances)
 
     def test_tokens(self):
-        assert len(self.instances) == 7
+        self.assertEqual(len(self.instances), 9)
