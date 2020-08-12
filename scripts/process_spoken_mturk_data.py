c1ed5db25dd359c3ccb1ebf8857d7c8a3ce9c82b
Nick Walker <nick@nickwalker.us>
2020-08-11 18:14:23 -0700

    Process transcripts for spoken paraphrases
    Fix a bug that could cause commands that shared the same index as Rejected HITs to be discarded
    Normalize and tokenize paraphrase text

--- scripts/process_spoken_mturk_data.py
+++ scripts/process_spoken_mturk_data.py
@@ -1,20 +1,39 @@
 import glob
+import os
 import re
 
 import audioread as audioread
 from nltk.metrics.distance import edit_distance, jaccard_distance
 import pandas as pd
 import hashlib
-import contextlib
 
-from tinytag import TinyTag
+from nltk.tokenize import word_tokenize
 
 paraphrasings = []
 new = []
 
+
 def get_file_duration(filename):
-    with audioread.audio_open("saved_audio/"+ filename) as f:
+    with audioread.audio_open("saved_audio/" + filename) as f:
         return sum(map(len, f)) / f.samplerate / f.channels
+
+
+def load_transcript(filename):
+    path = "transcripts/" + filename
+    if not os.path.isfile(path):
+        return ""
+    with open(path) as f:
+        return f.readline().strip()
+
+
+def clean_text(text):
+    # split into words
+
+    tokens = word_tokenize(text)
+    # remove all tokens that are not alphabetic
+    words = [word for word in tokens if word.isalpha()]
+    return " ".join(words)
+
 
 def process_turk_files(paths, filter_rejected=True):
     print("Processing paths: {}".format(str(paths)))
@@ -25,45 +44,71 @@ def process_turk_files(paths, filter_rejected=True):
         except StopIteration:
             return name
 
-    frame = pd.concat([pd.read_csv(path, na_filter=False) for path in paths])
+    # Ignore index so that we get a new ID per row, instead of numbers by file
+    frame = pd.concat([pd.read_csv(path, na_filter=False) for path in paths], ignore_index=True)
     if filter_rejected:
         frame.drop(frame[frame["AssignmentStatus"] == "Rejected"].index, inplace=True)
-
+    frame.sort_values(by="Input.hitid", inplace=True, ignore_index=True)
     data_views = []
-    for n in range(5):
+    # Count the occurences of Input.command. Sometimes we issue batches with different numbers
+    num_commands = frame.filter(regex='^Input.command', axis=1).shape[1]
+    for n in range(num_commands):
         columns = ["Input.command" + str(n), "Answer.paraphrase" + str(n), "WorkerId", "Input.hitid"]
         data_views.append(frame[columns].rename(columns=drop_trailing_num))
     paraphrases = pd.concat(data_views)
-    paraphrases.sort_values(by="Answer.paraphrase", inplace=True)
-    paraphrases["commandhash"] = paraphrases["Input.command"].apply(lambda command: hashlib.sha256(bytes(command, "utf-8")).hexdigest()[:6])
-    paraphrases["filename"] = paraphrases["Input.hitid"].astype(str) + "-" + paraphrases["commandhash"] + ".ogg"
-    #paraphrases["duration"] = paraphrases["filename"].apply(get_file_duration)
+    # We loaded many different files. Some may not have as many command columns as others
+    paraphrases.dropna(inplace=True)
+    paraphrases["Answer.paraphrase"] = paraphrases["Answer.paraphrase"].str.lower().apply(clean_text)
+
     new_views = []
-    for n in range(1):
-        new_views.append(frame[["Answer.newcommand" + str(n), "WorkerId"]].rename(columns=drop_trailing_num))
+    num_new_commands = frame.filter(regex='^Answer.newcommand', axis=1).shape[1]
+    for n in range(num_new_commands):
+        new_views.append(
+            frame[["Answer.newcommand" + str(n), "WorkerId", "Input.hitid"]].rename(columns=drop_trailing_num))
     new = pd.concat(new_views)
-    new.sort_values(by="Answer.newcommand", inplace=True)
+    new["Answer.newcommand"] = new["Answer.newcommand"].str.lower().apply(clean_text)
+    # Drop all the extra input or answer fields except those you want to use for additional stats
     other_data = frame.drop(
-        columns=[c for c in frame.columns if ("Input" in c or "Answer" in c) and not (c == "Answer.comment")])
+        columns=[c for c in frame.columns if
+                 ("Input" in c or "Answer" in c) and c != "Answer.comment" and c != "Input.hitid"])
+
+    nice_names = {"Answer.paraphrase": "paraphrase", "Input.command": "command", "Answer.newcommand": "command",
+                  "Input.hitid": "hitid"}
+    paraphrases.rename(columns=nice_names, inplace=True)
+    new.rename(columns=nice_names, inplace=True)
+    other_data.rename(columns=nice_names, inplace=True)
     return paraphrases, new, other_data
+
+
+def load_transcripts(data):
+    data["commandhash"] = data["command"].apply(lambda command: hashlib.sha256(bytes(command, "utf-8")).hexdigest()[:6])
+    data["filename"] = data["hitid"].astype(str) + "-" + data["commandhash"] + ".ogg"
+    # paraphrases["duration"] = paraphrases["filename"].apply(get_file_duration)
+    data["transcript_filename"] = data["hitid"].astype(str) + "-" + data["commandhash"] + ".txt"
+    data["transcript"] = data["transcript_filename"].apply(load_transcript)
 
 
 paraphrasings, new, other_data = process_turk_files(glob.glob("Batch_*.csv"))
 
-paraphrasings["EditDistanceNormalized"] = paraphrasings.apply(
-    lambda row: edit_distance(row["Input.command"], row["Answer.paraphrase"]) / len(row["Input.command"]), axis=1)
-paraphrasings["EditDistance"] = paraphrasings.apply(
-    lambda row: edit_distance(row["Input.command"], row["Answer.paraphrase"]), axis=1)
-paraphrasings["JaccardDistance"] = paraphrasings.apply(
-    lambda row: jaccard_distance(set(row["Input.command"].split()), set(row["Answer.paraphrase"].split())), axis=1)
+load_transcripts(paraphrasings)
+load_transcripts(new)
 
+paraphrasings["EditDistanceNormalized"] = paraphrasings.apply(
+    lambda row: edit_distance(row["command"], row["paraphrase"]) / len(row["command"]), axis=1)
+paraphrasings["EditDistance"] = paraphrasings.apply(
+    lambda row: edit_distance(row["command"], row["paraphrase"]), axis=1)
+paraphrasings["JaccardDistance"] = paraphrasings.apply(
+    lambda row: jaccard_distance(set(row["command"].split()), set(row["paraphrase"].split())), axis=1)
+
+paraphrasings["TranscriptQuality"] = paraphrasings.apply(
+    lambda row: min(1, edit_distance(row["paraphrase"], row["transcript"]) / max(len(row["transcript"]), 1)), axis=1)
 print(
     "{:.2f} {:.2f} {:.2f}".format(paraphrasings["EditDistanceNormalized"].mean(), paraphrasings["EditDistance"].mean(),
                                   paraphrasings["JaccardDistance"].mean()))
 by_worker = paraphrasings.groupby(paraphrasings["WorkerId"])
 for name, group in by_worker:
     print(name)
-    for i, (original, paraphrase) in group[["Input.command", "Answer.paraphrase"]].iterrows():
+    for i, (original, paraphrase) in group[["command", "paraphrase"]].iterrows():
         print(original)
         print(paraphrase)
         print("")
@@ -72,12 +117,14 @@ turker_performance = pd.DataFrame()
 turker_performance["HITTime"] = other_data.groupby("WorkerId")["WorkTimeInSeconds"].mean()
 turker_performance["MeanNormalizedEditDistance"] = paraphrasings.groupby("WorkerId")["EditDistanceNormalized"].mean()
 turker_performance["MeanJaccardDistance"] = paraphrasings.groupby("WorkerId")["JaccardDistance"].mean()
+turker_performance["MeanTranscriptQuality"] = paraphrasings.groupby("WorkerId")["TranscriptQuality"].mean()
+turker_performance["HITIds"] = other_data.groupby("WorkerId")["hitid"].apply(lambda x: ','.join(map(str, x)))
 turker_performance["Comment"] = other_data.groupby("WorkerId")["Answer.comment"].apply(lambda x: ','.join(x))
 
 turker_performance.to_csv("turker_stats.txt", index=False)
 
 for _, (original, paraphrase, edit, jaccard) in paraphrasings[
-    ["Input.command", "Answer.paraphrase", "EditDistance", "JaccardDistance"]].iterrows():  # noqa
+    ["command", "paraphrase", "EditDistance", "JaccardDistance"]].iterrows():  # noqa
     print(original)
     print(paraphrase)
     print("dist: ed{:.2f} ja{:.2f}".format(edit, jaccard))
@@ -87,7 +134,7 @@ print("--------------")
 new_by_worker = new.groupby(new["WorkerId"])
 for name, group in new_by_worker:
     print(name)
-    for custom_utt in group["Answer.newcommand"]:
+    for custom_utt in group["command"]:
         print(custom_utt)
         print("")
 
@@ -95,13 +142,12 @@ print("{} workers provided {} paraphrases and {} new commands".format(len(by_wor
 
 rephrasings_dict = {}
 with open("paraphrasings.txt", 'w') as outfile:
-    for _, (utt, parse) in paraphrasings[["Answer.paraphrase", "Input.command"]].sort_values(
-            by="Answer.paraphrase").iterrows():
+    for _, (utt, parse) in paraphrasings[["paraphrase", "command"]].sort_values(
+            by="paraphrase").iterrows():
         outfile.write(utt + "\n")
         outfile.write(parse + "\n")
         rephrasings_dict[utt] = parse
 
-
 with open("custom.txt", 'w') as outfile:
-    for utt in new["Answer.newcommand"].sort_values():
+    for utt in new["command"].sort_values():
         outfile.write(utt + "\n")
